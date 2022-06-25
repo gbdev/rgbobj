3e63f0713e981a2308e11cb0bb93ac98fb0d7898
ISSOtm <eldredhabert0@gmail.com>
2022-06-25 16:21:07 +0200

    Sync doc URL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/rgbobj/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/rgbobj/0.2.0")]
 
 use rgbds_obj::{Node, NodeType, NodeWalkError, Object};
 use std::convert::{Infallible, TryFrom};
